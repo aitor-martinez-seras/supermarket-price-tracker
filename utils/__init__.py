6ef0b9b4160ffb372e9e35408df13a807728ebe4
110414 <aitor.martinez@tecnalia.com>
2022-11-19 11:48:21 +0100

    Merge __init__.py from ray branch

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,8 @@
 from .io_utils import *
 from .parsers import *
+from .retriever import *
+
+
+EROSKI_RET = Retriever([('class','price-now'),('itemprop','price')])
+MERCADONA = Retriever([('class','price-now'),('itemprop','price')])  #TODO: Check mercadona's keys
+ALL_RETS = {'eroski': EROSKI_RET, 'mercadona': MERCADONA}
