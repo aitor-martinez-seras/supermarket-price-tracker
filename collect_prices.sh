68ddebf6dc256fdaafb7550e0bac239b1c2291ec
110414 <aitor.martinez@tecnalia.com>
2023-01-22 20:55:16 +0100

    fixed paths of the sh

--- collect_prices.sh
+++ collect_prices.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/supermarket-price-tracker/prices-venv/bin/python3 src/price_tracker/main.py
+/home/pi/supermarket-price-tracker/prices-venv/bin/python3 /home/pi/supermarket-price-tracker/src/price_tracker/main.py
